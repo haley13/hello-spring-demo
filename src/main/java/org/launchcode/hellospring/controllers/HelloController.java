e8ed6cecef64f54f4aba0aea08640db5cc02f42a
haley13 <allickhaley@gmail.com>
2021-05-27 17:03:35 -0500

    created template

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -8,16 +8,18 @@ import org.springframework.web.bind.annotation.*;
  */
 @Controller
 public class HelloController {
+
+    //Responds to /hello?name=LaunchCode
     @RequestMapping(value= "hello", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
     public String hello(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
+    //Responds to /hello/LaunchCode
     @GetMapping("hello/{name}")
     @ResponseBody
     public String helloAgain(@PathVariable String name) {
         return "Hello, " + name + "!";
-
     }
 
     @GetMapping("form")
