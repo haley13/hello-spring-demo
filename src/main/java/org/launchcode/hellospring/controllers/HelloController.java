71e128f1ccbbcd83f39fc5ff8deeb891ecad8164
haley13 <allickhaley@gmail.com>
2021-05-28 15:33:59 -0500

    forms

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -24,4 +24,16 @@ public class HelloController {
     public String helloAgain(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
+
+    @GetMapping("form")
+    public String helloForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action = 'hello' method = 'post'>" + // submit a request to /hello
+                "<input type = 'text' name = 'name' >" +
+                "<input type = 'submit' value = 'Greet Me!' >" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
 }
