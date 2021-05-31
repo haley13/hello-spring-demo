ab87fe8be7ea7b3115ea6ed61e2cb84a1d508eb1
haley13 <allickhaley@gmail.com>
2021-05-31 15:16:32 -0500

    controller exercises

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -5,33 +5,30 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
 //    @GetMapping("hello")
 //    public String hello() {
 //        return "Hello, Spring!";
 //    }
-    @GetMapping("goodbye")
-    public String goodbye(){
-        return "Goodbye, Spring!";
-    }
-    @GetMapping("hello")
-    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
     public String helloWithQueryParam(@RequestParam String name){
         return "Hello, " + name + "!";
     }
-    @GetMapping(" {name}")
-    public String helloAgain(@PathVariable String name) {
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
-    @GetMapping("form")
+    @RequestMapping(method = RequestMethod.GET)
     public String helloForm() {
         return "<html>" +
                 "<body>" +
                 "<form method= 'POST' action = '/hello'>" + // submit a request to /hello
                 "<input type = 'text' name = 'name' >" +
-                "<input type = 'text' language= 'language" +
+                //"<input type = 'text' language= 'language" +
                 "<input type = 'submit' value = 'Greet Me!' >" +
                 "</form>" +
                 "</body>" +
